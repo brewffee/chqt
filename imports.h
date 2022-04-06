d48d9f23dcc15776532a5b13b39fa00017e89058
geixco <geixco@protonmail.com>
2022-04-06 00:54:33 -0400

    place imports in separate file

---
+++ imports.h
@@ -0,0 +1,18 @@
+#ifndef imports_h
+#define imports_h
+#endif
+
+#include <QStandardItemModel>
+#include <QStandardItem>
+#include <QApplication>
+#include <QMainWindow>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QTreeView>
+#include <QToolBar>
+#include <QMenuBar>
+#include <QWidget>
+#include <QWindow>
+#include <QLabel>
+#include <QList>
