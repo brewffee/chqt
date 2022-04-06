f4e0d9ccdb46cc9fb25eac005066164be5e3deb0
geixco <geixco@protonmail.com>
2022-04-05 21:34:28 -0400

    add right sidebar content

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 #include <QMenuBar>
 #include <QWidget>
 #include <QLabel>
+#include <QList>
+
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -37,20 +39,22 @@ int main(int argc, char *argv[]) {
     QStandardItemModel *model = new QStandardItemModel();
 
     // typically the tree would be populated from a database
-    QStandardItem *parent = new QStandardItem("parent");
-    QStandardItem *child = new QStandardItem("child");
-    parent -> appendRow(child);
-    model -> appendRow(parent);
+    QStandardItem *server = new QStandardItem("Default Server");
+    QStandardItem *channel = new QStandardItem("Default Channel");
+    server -> appendRow(channel);
+    model -> appendRow(server);
+
 
     treeView -> setModel(model);
     treeView -> setHeaderHidden(true);
     treeView -> expandAll();
     treeView -> setEditTriggers(QAbstractItemView::NoEditTriggers);
+    treeView -> setCurrentIndex(model -> indexFromItem(channel));
 
     sidebarL.setFixedWidth(content.width() / 6);
 
     QVBoxLayout *sidebarL_layout = new QVBoxLayout();
-    QLabel *headerL = new QLabel("Sidebar Left");
+    QLabel *headerL = new QLabel("Connections");
     headerL -> setAlignment(Qt::AlignCenter);
     sidebarL_layout -> addWidget(headerL);
     sidebarL_layout -> addWidget(treeView);
@@ -68,14 +72,51 @@ int main(int argc, char *argv[]) {
     layout.addWidget(&main);
 
     // RIGHT SIDEBAR
+    QList<QStandardItem *> items;
+
+    // types of possible members
+    QStandardItem *op = new QStandardItem("@Operator");
+    QStandardItem *moderator = new QStandardItem("+Moderator");
+    QStandardItem *member = new QStandardItem("Member");
+    QStandardItem *buddy = new QStandardItem("Buddy");
+    QStandardItem *muted = new QStandardItem("Muted");
+    QStandardItem *blocked = new QStandardItem("Blocked");
+    QStandardItem *self = new QStandardItem("You");
+
+    // give each member type a font style
+    buddy -> setForeground(QBrush(QColor(2, 200, 0)));
+    muted -> setForeground(QBrush(QColor(200, 200, 200)));
+    blocked -> setForeground(QBrush(QColor(255, 0, 0)));
+
+    // my system's default font is Product Sans, need to
+    // find out how to use default font properties later
+    self -> setFont(QFont("Product Sans", 12, QFont::Bold));
+
+    std::array <QStandardItem *, 7> member_types = {op, moderator, member, buddy, muted, blocked, self};
+
+    for (auto &item : member_types) {
+        items.append(item);
+    }
+
+    QStandardItemModel *member_model = new QStandardItemModel();
+    member_model -> appendColumn(items);
+
+    // create a tree view from the model
+    QTreeView *member_tree = new QTreeView();
+    member_tree -> setModel(member_model);
+    member_tree -> setHeaderHidden(true);
+    member_tree -> expandAll();
+    member_tree -> setEditTriggers(QAbstractItemView::NoEditTriggers);
+ 
     sidebarR.setFixedWidth(content.width() / 6);
 
     QVBoxLayout *sidebarR_layout = new QVBoxLayout();
-    QLabel *headerR = new QLabel("Sidebar Right");
+    QLabel *headerR = new QLabel("Members");
     headerR -> setAlignment(Qt::AlignCenter);
     sidebarR_layout -> addWidget(headerR);
+    sidebarR_layout -> addWidget(member_tree);
     sidebarR.setLayout(sidebarR_layout);
-
+    
     layout.addWidget(&sidebarR);
 
     // finishing up
@@ -87,4 +128,3 @@ int main(int argc, char *argv[]) {
     window -> show();
     return app.exec();
 }
-
